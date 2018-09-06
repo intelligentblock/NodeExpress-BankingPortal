b951d29fb896dec2aed96955bc17bd1b7de9c8a1
intelligentblock <39675871+intelligentblock@users.noreply.github.com>
2018-09-05 22:37:19 -0400

    Completed the module

--- src/app.js
+++ src/app.js
@@ -4,9 +4,39 @@ const express=require('express');
 const app=express();
 
 app.set('views',path.join(__dirname,'views'));
+
 app.set('views engine','ejs');
+
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',(req,res)=>res.render('index',{title:'Index'}));
+const accountData=fs.readFileSync(
+  path.join(__dirname,'json','accounts'),'utf8'    
+);
+const accounts=json.parse('accountData');
 
-app.listen(3000,()=>console.log('log on port 3000'));
+const userData=fs.readFileSync(
+    path.join(__dirname,'json','users'),'utf8'
+);
+const users=json.parse('userData');
+
+app.get('/',(req,res)=>{res.render('index',{title:'Accounts Summary',accounts});
+});
+
+
+app.get('/savings',(req,res)=>{
+    res.render('account',{account:accounts.savings});
+});
+
+app.get('/checking',(req,res)=>{
+    res.render('account',{account:accounts.checking});
+});
+
+app.get('/credit',(req,res)=>{
+    res.render('account',{account:accounts.credit});
+});
+
+app.get('/profile',(req,res)=>{
+    res.render('profile',{user:user[0]});
+});
+
+app.listen(9001, () =>console.log('log on port 5000'));
